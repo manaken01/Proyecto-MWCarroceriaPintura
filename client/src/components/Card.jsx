6e5a1c7304aa7df5c598b5ee9b9b4e1ef469ec3c
Mariangeles <mariangcv25@estudiantec.cr>
2023-12-27 00:52:27 -0600

    divider heart

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import whatsapp from '../assets/whatsapp.png'
 import HeartButton from './HeartButton';
+import Divider from './Divider';
+
 //set parapmetersd from function
 function Card() {
-    
+
     return (
         <div class="card mb-3" style={{ maxWidth: '500px', backgroundColor: "#F9F9F9" }}>
             <div class="row g-0">
@@ -16,7 +18,12 @@ function Card() {
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title" style={{ color: '#000000' }}>Repuesto nombre </h5><HeartButton />
+                    <div class="d-flex justify-content-between align-items-center">
+                    <h5 class="card-title" style={{ color: '#000000' }}>Repuesto nombre </h5>
+                    <HeartButton />
+                    
+                </div>
+                <Divider />
                         <p class="card-text" ><strong>Carro: NISSAN SENTRA 2000-2006 (B15)</strong></p>
                         <p class="card-text" style={{ marginBottom: '0', color: '#C80B16' }}><strong>Precio: </strong> ₡ 40000</p>
                         <p class="card-text" style={{ marginBottom: '0', color: '#000000' }}><strong>Categoría: </strong>Dash</p>
@@ -25,9 +32,9 @@ function Card() {
                         <p class="card-text" style={{ marginBottom: '0', color: '#000000' }}><strong>Versión: </strong>Japón</p>
                         <p class="card-text"><strong>Generación: </strong> 2</p>
 
-                        <button type="button" class="btn btn-danger " style={{ textAlign:'left',backgroundColor: '#C80B16', width: '100%' , height: '5%' }}>
-                        
-                        <img src={whatsapp}style={{height: '95%', width: '10%', marginRight: '4%'}}></img>
+                        <button type="button" class="btn btn-danger " style={{ textAlign: 'left', backgroundColor: '#C80B16', width: '100%', height: '5%' }}>
+
+                            <img src={whatsapp} style={{ height: '95%', width: '10%', marginRight: '4%' }}></img>
                             Preguntar vía Whatsapp
                         </button>
                     </div>
