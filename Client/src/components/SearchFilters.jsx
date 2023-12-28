583cc6798f089a9e8300d010731075183bd66196
José Mauricio Muñoz Morera <jmau2602@gmail.com>
2023-12-28 00:59:19 -0600

    searchbar

--- Client/src/components/SearchFilters.jsx
+++ Client/src/components/SearchFilters.jsx
@@ -13,7 +13,7 @@ const SearchFilters = () => {
         <div className="container" style={{ marginTop: '5%', marginBottom: '5%', backgroundColor: "#F9F9F9", boxShadow: "#E3E3E3 3px 3px 3px" }}>
             <div className="row align-items-center">
                 <div className="col-12">
-                    <div className="input-group mb-3 align-items-center" style={{ paddingTop: '2%', width: '50%', minWidth: '250px' }}>
+                    <div className="input-group mb-3 align-items-center" style={{ paddingTop: '2%', width: '50%', minWidth: '250px', }}>
                         <div className="input-group-prepend">
                             <span className="input-group-text" id="basic-addon1">🔍</span>
                         </div>
@@ -24,7 +24,7 @@ const SearchFilters = () => {
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="row ">
                 {dropdowns.map((dropdown, index) => (
                     <div className="col" key={index}>
                         <p style={{ fontWeight: 'bold', marginBottom: '5px' }}>{dropdown.label}</p>
@@ -42,12 +42,12 @@ const SearchFilters = () => {
                     </div>
                 ))}
             </div>
-            <div className="row">
-    <div className="col text-left">
-        <button className="btn btn-outline-secondary">Left Button</button>
+            <div className="row mt-3 mb-3" style={{ paddingBottom: '2%' }}>
+    <div className="col d-flex justify-content-start">
+        <button className="btn btn-outline-secondary" style={{width: '10%', backgroundColor: '#C80B16', borderColor: '#C80B16', color: 'white', minWidth: '100px'}}>Filtrar</button>
     </div>
-    <div className="col text-right">
-        <button className="btn btn-outline-secondary ml-auto">Right Button</button>
+    <div className="col d-flex justify-content-end">
+        <button className="btn btn-outline-secondary" style={{ backgroundColor: '#C80B16', borderColor: '#C80B16', color: 'white' }}>Agregar nuevo carro</button>
     </div>
 </div>
         </div>
