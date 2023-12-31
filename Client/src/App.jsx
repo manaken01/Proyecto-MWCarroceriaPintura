3f317e4173c9055009f719362c1e15fbf7fbe2e7
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-12-30 22:35:32 -0600

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import StartScreen from './components/full screens MCV/StartScreen';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Calendar from './components/objects/Calendar';
-import Navbar from './components/objects/Navbar';
 import Information from './components/full screens MCV/Information';
 
 function App() {
