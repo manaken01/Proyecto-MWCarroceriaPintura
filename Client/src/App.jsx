cd29233b29af644203265d476668367f91245727
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-12-30 22:46:55 -0600

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import StartScreen from './components/full screens MCV/StartScreen';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Information from './components/full screens MCV/Information';
-
+import Calendar from './components/objects/Calendar'
 function App() {
   return (
     <div>
