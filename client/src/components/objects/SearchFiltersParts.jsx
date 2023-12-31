5c26b482fdf8cb0774ba97fa3f36728a7d329735
Mariangeles <mariangcv25@estudiantec.cr>
2023-12-30 22:43:09 -0600

    si

--- client/src/components/objects/SearchFiltersParts.jsx
+++ client/src/components/objects/SearchFiltersParts.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 
-const SearchFilters = () => {
+const SearchFiltersParts = () => {
     const dropdowns = [
         { label: 'Marca:', items: ['Toyota', 'Honda', 'Ford', 'Chevrolet'] },
-        { label: 'Año:', items: ['2000', '2005', '2010', '2015'] },
-        { label: 'Body Shape:', items: ['Sedan', 'SUV', 'Coupe', 'Hatchback'] },
-        { label: 'Transmisión:', items: ['Manual', 'Automático', 'CVT', 'Dual-Clutch'] },
+        { label: 'Carro:', items: ['2000', '2005', '2010', '2015'] },
+        { label: 'Categoría:', items: ['Sedan', 'SUV', 'Coupe', 'Hatchback'] },
+        { label: 'Repuestos:', items: ['Manual', 'Automático', 'CVT', 'Dual-Clutch'] },
     ];
 
     return (
-        <div className="container" style={{ marginBottom: '4%', backgroundColor: "#F9F9F9", boxShadow: "#E3E3E3 3px 3px 3px" }}>
+        <div className="container" style={{ marginTop: '5%', marginBottom: '5%', backgroundColor: "#F9F9F9", boxShadow: "#E3E3E3 3px 3px 3px" }}>
             <div className="row align-items-center">
                 <div className="col-12">
                     <div className="input-group mb-3 align-items-center" style={{ paddingTop: '2%', width: '50%', minWidth: '250px', }}>
                         <div className="input-group-prepend">
                             <span className="input-group-text" id="basic-addon1">🔍</span>
                         </div>
-                        <input type="text" className="form-control" placeholder="Buscar repuestos" aria-label="searchCar" aria-describedby="basic-addon1" />
+                        <input type="text" className="form-control" placeholder="Buscar carro" aria-label="searchCar" aria-describedby="basic-addon1" />
                         <div className="input-group-append">
                             <button className="btn btn-outline-secondary" type="button" id="button-addon2" style={{ width:'200%' , backgroundColor: '#C80B16', borderColor: '#C80B16', color: 'white',marginLeft: '20%'}}>Buscar</button>
                         </div>
@@ -54,4 +54,4 @@ const SearchFilters = () => {
     );
 };
 
-export default SearchFilters;
+export default SearchFiltersParts;
