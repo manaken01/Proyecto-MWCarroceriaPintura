69554155cf79f85f80fd730a48297041d3a14ec6
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-12-30 22:08:33 -0600

    Calendario

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import StartScreen from './components/full screens MCV/StartScreen';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Calendar from './components/objects/Calendar';
+import Navbar from './components/objects/Navbar';
+
 function App() {
   return (
     <div>
-      <StartScreen />
+      <Navbar/>
+      <Calendar/>
       
     </div>
   );
--- client/src/components/objects/Calendar.jsx
+++ client/src/components/objects/Calendar.jsx
@@ -1,41 +1,73 @@
-import React, { useState } from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import React, { useState } from 'react';
+import { formatDate } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { INITIAL_EVENTS, createEventId } from '../event-utils';
 
 function Calendar() {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([]);
 
-  const handleDateClick = (arg) => {
-    const dateStr = arg.dateStr;
-    setSelectedDate(dateStr);
-    setModalOpen(true);
+  const handleWeekendsToggle = () => setWeekendsVisible(prev => !prev);
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event');
+    if (title) selectInfo.view.calendar.addEvent({
+      id: createEventId(),
+      title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay
+    });
   };
 
-
-  const handleSaveEvent = () => {
-    // Aquí puedes manejar la lógica para guardar el evento
-    console.log("Evento guardado:", selectedDate);
-    handleCloseModal();
+  const handleEventClick = (clickInfo) => {
+    if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
   };
+
+  const handleEvents = (events) => setCurrentEvents(events);
+
+  const renderEventContent = (eventInfo) => (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+
+  const renderSidebarEvent = (event) => (
+    <li key={event.id}>
+      <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+      <i>{event.title}</i>
+    </li>
+  );
 
   return (
-    <div>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView={"dayGridMonth"}
-        headerToolbar={{
-          start: "today prev,next",
-          center: "title",
-          end: "dayGridMonth,timeGridWeek,timeGridDay",
-        }}
-        height={"90vh"}
-        selectable={true}
-        select={handleDateClick}
-        events={[
-          // Aquí puedes proporcionar eventos preexistentes si los tienes
-        ]}
-      />
+    <div className='demo-app'>
+      <div className='demo-app-main' style={{ maxWidth: '800px', margin: '0 auto' }}>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          initialView='dayGridMonth'
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={weekendsVisible}
+          initialEvents={INITIAL_EVENTS}
+          select={handleDateSelect}
+          eventContent={renderEventContent}
+          eventClick={handleEventClick}
+          eventsSet={handleEvents}
+        />
+      </div>
     </div>
   );
 }
