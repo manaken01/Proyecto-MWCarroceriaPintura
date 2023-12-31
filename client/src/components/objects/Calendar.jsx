2003c823d13bae5fa2dcc99fc7f6b84f9fc5f55a
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-12-30 22:18:45 -0600

    Calendario que usare de base

--- client/src/components/objects/Calendar.jsx
+++ client/src/components/objects/Calendar.jsx
@@ -4,7 +4,7 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { INITIAL_EVENTS, createEventId } from '../event-utils';
+import { INITIAL_EVENTS, createEventId } from '../resources/event-utils';
 
 function Calendar() {
   const [weekendsVisible, setWeekendsVisible] = useState(true);
@@ -46,8 +46,30 @@ function Calendar() {
   );
 
   return (
-    <div className='demo-app'>
-      <div className='demo-app-main' style={{ maxWidth: '800px', margin: '0 auto' }}>
+    <div className='demo-app' style={{ display: 'flex', minHeight: '100%', fontFamily: 'Arial, Helvetica Neue, Helvetica, sans-serif', fontSize: '14px' }}>
+      <div className='demo-app-sidebar' style={{ width: '300px', lineHeight: '1.5', background: '#eaf9ff', borderRight: '1px solid #d3e2e8' }}>
+        <div className='demo-app-sidebar-section' style={{ padding: '2em' }}>
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className='demo-app-sidebar-section' style={{ padding: '2em' }}>
+          <label>
+            <input type='checkbox' checked={weekendsVisible} onChange={handleWeekendsToggle} />
+            toggle weekends
+          </label>
+        </div>
+        <div className='demo-app-sidebar-section' style={{ padding: '2em' }}>
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>
+            {currentEvents.map(renderSidebarEvent)}
+          </ul>
+        </div>
+      </div>
+      <div className='demo-app-main' style={{ flexGrow: '1', padding: '3em' }}>
         <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           headerToolbar={{
