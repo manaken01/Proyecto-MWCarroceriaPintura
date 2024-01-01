2921dcf24a360253dffd125886bda7e4d73dbd35
José Mauricio Muñoz Morera <jmau2602@gmail.com>
2024-01-01 16:27:35 -0600

    óptimo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import PartsForm from './components/forms/PartsForm';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchFilters from './components/objects/SearchFilters';
 
 function App() {
   return (
     <div>
-      <PartsForm />
+      <SearchFilters />
       
     </div>
   );
