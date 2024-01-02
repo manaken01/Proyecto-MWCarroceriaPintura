a59eef790c17460559e74e1cb429c8907eeefe99
manaken01 <82286286+manaken01@users.noreply.github.com>
2024-01-01 18:23:14 -0600

    consulta userType

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,11 +3,11 @@ import PartsForm from './components/forms/PartsForm';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchFilters from './components/objects/SearchFilters';
-
+import Calendar from './components/objects/Calendar';
 function App() {
   return (
     <div>
-      <SearchFilters />
+      <Calendar />
       
     </div>
   );
--- server/mainController.py
+++ server/mainController.py
@@ -1,7 +1,12 @@
 from controller.adminBrand import *
+from controller.adminUser import *
 from model.Brand import *
 
 class mainController:
+
     def createBrand(brand,connection,cursor):
         return (adminBrand.createBrand(brand,connection,cursor))
+    
+    def getUserTypes(cursor):
+        return (adminUser.getUserTypes(cursor))
         
