b70400e646229231b2115553e572ef41ef8ea817
José Mauricio Muñoz Morera <jmau2602@gmail.com>
2024-01-07 23:23:35 -0600

    cruds carsell y user

---
+++ server/model/CarSell.py
@@ -0,0 +1,13 @@
+class CarSell:
+    def __init__(self, year, color, transmission, licensePlate, bodyShape, version, passengers, model, price, activated, idBrand):
+        self.year = year
+        self.color = color
+        self.transmission = transmission
+        self.licensePlate = licensePlate
+        self.bodyShape = bodyShape
+        self.version = version
+        self.passengers = passengers
+        self.model = model
+        self.price = price
+        self.activated = activated
+        self.idBrand = idBrand
