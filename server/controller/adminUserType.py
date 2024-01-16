184f1c56259a8f0ff1534932e6fe8c79fafab2ee
José Mauricio Muñoz Morera <jmau2602@gmail.com>
2024-01-16 10:01:31 -0600

    usertype

--- server/controller/adminUserType.py
+++ server/controller/adminUserType.py
@@ -6,12 +6,9 @@ import json
 class adminUserType:
     def getUserTypes(connection,cursor):
         try: 
-            cursor = connection.cursor(dictionary=True)
             sql = "Select * FROM userType"
             cursor.execute(sql)
             result = cursor.fetchall()
-            cursor.close()
-            cursor = connection.cursor(dictionary=True)
             return result
         except mysql.connector.Error as error:
             print("Failed to execute stored procedure: {}".format(error))
